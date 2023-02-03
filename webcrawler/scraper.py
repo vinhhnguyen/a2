f1949a1b511a812bcffefe4f4cc961e3b6194656
Vinh Nguyen <32783985+vinhhnguyen@users.noreply.github.com>
2023-02-02 17:29:40 -0800

    Add files via upload

---
+++ webcrawler/scraper.py
@@ -0,0 +1,55 @@
+import re
+from bs4 import BeautifulSoup
+from urllib.parse import urldefrag, urlparse, urljoin
+
+
+def scraper(url, resp):
+    links = extract_next_links(url, resp)
+    return [link for link in links if is_valid(link)]
+
+def extract_next_links(url, resp):
+    # Implementation required.
+    # url: the URL that was used to get the page
+    # resp.url: the actual url of the page
+    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
+    # resp.error: when status is not 200, you can check the error here, if needed.
+    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
+    #         resp.raw_response.url: the url, again
+    #         resp.raw_response.content: the content of the page!
+    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
+    soup = BeautifulSoup(resp.raw_response.content, 'lxml')
+    links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if type(href) != type(None): 
+            absoluteLink = urljoin(resp.raw_response.url, href)
+            unfragAbsLink = urldefrag(absoluteLink)[0]
+            links.append(unfragAbsLink)
+    print(links)
+
+def is_valid(url):
+    # Decide whether to crawl this url or not. 
+    # If you decide to crawl it, return True; otherwise return False.
+    # There are already some conditions that return False. 
+    try:
+        parsed = urlparse(url)
+        if parsed.hostname == None:
+            return False
+        domains = False
+        if (".ics.uci.edu" in parsed.hostname) or (".cs.uci.edu" in parsed.hostname) or (".informatics.uci.edu" in parsed.hostname) or (".stat.uci.edu" in parsed.hostname):
+            domains = True 
+        if parsed.scheme not in set(["http", "https"]) or not domains:
+            return False
+        return not re.match(
+            r".*\.(css|js|bmp|gif|jpe?g|ico"
+            + r"|png|tiff?|mid|mp2|mp3|mp4"
+            + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
+            + r"|ps|eps|tex|ppt|pptx|doc|docx|xls|xlsx|names"
+            + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|dmg|iso"
+            + r"|epub|dll|cnf|tgz|sha1"
+            + r"|thmx|mso|arff|rtf|jar|csv"
+            + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+
+    except TypeError:
+        print ("TypeError for ", parsed)
+        raise
